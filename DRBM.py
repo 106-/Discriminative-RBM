a27b6b13d7e722bc48086b8df827e94449b65ebb
106 <Segmentation-fault@yandex.ua>
2018-07-02 17:28:16 +0900

    numpyで行列の計算をするようにした

--- DRBM.py
+++ DRBM.py
@@ -3,6 +3,9 @@
 
 import math
 import random
+import numpy as np
+import json
+import logging
 
 class DRBM:
 
@@ -11,180 +14,148 @@ class DRBM:
         self.num_hidden = num_hidden
         self.num_class = num_class
         # Xavierの初期値
-        sqrt_max_num_of_node = math.sqrt(max(num_visible, num_hidden, num_class))
-        self.weight_v = [[random.gauss(0, 1/sqrt_max_num_of_node) for j in range(num_hidden)] for i in range(num_visible)]
-        self.weight_w = [[random.gauss(0, 1/sqrt_max_num_of_node) for k in range(num_class)] for j in range(num_hidden)]
-        self.bias_c = [random.gauss(0, 1/sqrt_max_num_of_node) for j in range(num_hidden)]
-        self.bias_b = [random.gauss(0, 1/sqrt_max_num_of_node) for k in range(num_class)]
+        sq_node = 1 / math.sqrt(max(num_visible, num_hidden, num_class))
+        self.weight_v = sq_node * np.random.randn(self.num_visible, self.num_hidden)
+        self.weight_w = sq_node * np.random.randn(self.num_hidden, self.num_class) 
+        self.bias_c = sq_node * np.random.randn(self.num_hidden)
+        self.bias_b = sq_node * np.random.randn(self.num_class)
 
-    @staticmetho
-    def _sigmoid(x):
-        return 1/(1+math.exp(-x))
-
-    # log(1+e^x)を安全に計算する
     @staticmethod
-    def _log_exp(x):
-        if x<0:
-            return math.exp(x)
+    def _sigmoid(x):
+        # オーバーフロー対策
+        if -x > 709:
+            return 1.2167807506234229e-308
+        return 1/(1+np.exp(-x))
+
+    @staticmethod
+    def _exp(x):
+        if x>0:
+            return x + np.log1p( np.exp(-x) )
         else:
-            return (x+math.log(1+math.exp(-x)))
+            return np.log1p( np.exp(x) )
 
     # one-of-k表現のベクトルを生成
-    @staticmethod
-    def one_of_k(size, number):
+    def one_of_k(self, number):
         # "One of K" -> "ok"
-        ok = [0 for i in range(size)]
-        ok[number] = 1
+        ok = np.zeros(self.num_class)
+        ok.put(number, 1)
         return ok
 
     # 31PにあるA_jを計算
-    def _calc_A(self, input_vector, one_of_k, j=None, update=False):
-        def func(j):
-            weight_v_under_j = list(map(lambda x:x[j], self.weight_v))
-            weight_w_under_j = self.weight_w[j]
-            sum_wt = 0
-            for w,t in zip(weight_w_under_j, one_of_k):
-                sum_wt += w*t
-            sum_vx = 0
-            for v,x in zip(weight_v_under_j, input_vector):
-                sum_vx += v*x
-            return self.bias_c[j] + sum_wt + sum_vx
-    
-        a = None
+    def _calc_A(self, j, input_vector, one_of_k):
+        return self.bias_c[j] * np.dot(self.weight_w[j], one_of_k) * np.dot(self.weight_v[:,j], input_vector)
 
-        if update == True:
-            # jがNoneなら全てのA_jを計算
-            if j == None:
-                a = []
-                for j in range(self.num_hidden):
-                    a.append(func(j))
-                self._old_a = a
-            # jが整数ならその値に対するA_jを計算
-            elif isinstance(j, int):
-                a = func(j)
-                self._old_a[j] = a
+    def probability(self, input_vector, k=None):
+        vexp = np.vectorize(self._exp)
+        A_matrix = np.array([[self._calc_A(m, input_vector, self.one_of_k(k)) for m in range(self.num_hidden)] for k in range(self.num_class)])
+        log_a = np.log( np.ones((self.num_class, self.num_hidden)) + vexp(-A_matrix) )
+        sum_under_j = np.sum( A_matrix + log_a , axis=1)
+
+        energies = vexp( self.bias_b + sum_under_j )
+        # nc -> Normalize Constant
+        nc = np.sum(energies)
+        probs = energies / nc
+
+        if k == None:
+            return probs
         else:
-            if j == None:
-                a = self._old_a
-            elif isinstance(j, int):
-                a = self._old_a[j]
-            
-        return a
-
-    def probability(self, input_vector, k=None, update=False):
-        if update == True:
-            sum_exp_energies = []
-            for k_c in range(self.num_class):
-                A = self._calc_A(input_vector, self.one_of_k(self.num_class, k_c), update=True)
-                sum_ln = sum(list(map(lambda x: 1+math.exp(x), A)))
-                sum_exp_energies.append(math.exp(self.bias_b[k_c]) + sum_ln)
-            # nc -> Normalize Constant
-            nc = sum(sum_exp_energies)
-            probs = list(map(lambda x: x/nc, sum_exp_energies))
-
-            if k==None:
-                self._old_prob = probs
-                return probs
-            elif isinstance(k, int):
-                self._old_prob[k] = probs[k]
-                return probs[k]
-        else:
-            if k == None:
-                return self._old_prob
-            elif isinstance(k, int):
-                return self._old_prob[k]
+            return probs[k]
 
     def _differential_b(self, training_datas, training_answers):
-        diff_b = []
-        for k in range(self.num_class):
-            sum_differ = 0
-            for t_u, x_u in zip(training_answers, training_datas):
-                sum_differ += t_u[k] - self.probability(x_u, k=k)
-            diff_b.append(sum_differ / len(training_datas))
+        probs_matrix = np.array([self.probability(x) for x in training_datas])
+        diff_b = np.sum(training_answers - probs_matrix, axis=0) / len(training_datas)
         return diff_b
     
     def _differential_w(self, training_datas, training_answers):
-        diff_w = [[None for k in range(self.num_class)] for j in range(self.num_hidden)]
-        for j in range(self.num_hidden):
-            for k in range(self.num_class):
-                sum_sig = 0
-                for t_u, x_u in zip(training_answers, training_datas):
-                    sum_sig += self._sigmoid(self._calc_A(x_u, self.one_of_k(self.num_class, k), j=j)) * (t_u[k] - self.probability(x_u, k=k))
-                diff_w[j][k] = sum_sig / len(training_datas)
+        vsigmoid = np.vectorize(self._sigmoid)
+
+        A_matrix = np.array([[[self._calc_A(m, x, self.one_of_k(k)) for k in range(self.num_class)] for x in training_datas] for m in range(self.num_hidden)])
+        probs_matrix = np.array([self.probability(x) for x in training_datas])
+
+        A_mul_difftp = vsigmoid(A_matrix) * (training_answers - probs_matrix)
+        diff_w = np.sum(A_mul_difftp, axis=1) / len(training_datas)
         return diff_w
     
     def _differential_c(self, training_datas, training_answers):
-        diff_c = []
-        for j in range(self.num_hidden):
-            sum_ = 0
-            for t_u, x_u in zip(training_answers, training_datas):
-                sum_data_sig = self._sigmoid(self._calc_A(x_u, t_u, j=j))
-                sum_sig = 0
-                for k in range(self.num_class):
-                    sum_sig += self._sigmoid(self._calc_A(x_u, self.one_of_k(self.num_class, k), j=j)) * self.probability(x_u, k=k)
-                sum_ += (sum_data_sig - sum_sig)
-            diff_c.append(sum_ / len(training_datas))
+        vsigmoid = np.vectorize(self._sigmoid)
+
+        A_matrix = np.array([[[self._calc_A(m, x, self.one_of_k(k)) for k in range(self.num_class)] for x in training_datas] for m in range(self.num_hidden)])
+        probs_matrix = np.array([self.probability(x) for x in training_datas])
+        sum_under_k = np.sum(A_matrix * probs_matrix, axis=2)
+
+        A_matrix_tx = np.array([[self._calc_A(m, training_datas[u], training_answers[u]) for u in range(len(training_datas))] for m in range(self.num_hidden)])
+        diff_c = np.sum( vsigmoid(A_matrix_tx) - sum_under_k, axis=1 ) / len(training_datas)
         return diff_c
     
     def _differential_v(self, training_datas, training_answers):
-        diff_v = [[None for j in range(self.num_hidden)] for i in range(self.num_visible)]
-        for i in range(self.num_visible):
-            for j in range(self.num_hidden):
-                sum_ = 0
-                for t_u, x_u in zip(training_answers, training_datas):
-                    sig = self._sigmoid(self._calc_A(x_u, t_u, j=j))
-                    sum_sig_p = 0
-                    for k in range(self.num_class):
-                        sum_sig_p += self._sigmoid(self._calc_A(x_u, self.one_of_k(self.num_class, k), j=j)) * self.probability(x_u, k=k)
-                    sum_ += x_u[i] * (sig - sum_sig_p)
-                diff_v[i][j] = sum_ / len(training_datas)
-        return diff_v            
-                
-    def train(self, training_datas, training_answers, test_datas, test_answers, learning_time, learning_rate=[0.01, 0.01, 0.01, 0.01], test_interval=100):
-        if not (self.num_visible == len(training_datas[0])):
-            print(len(training_datas[0]))
+        vsigmoid = np.vectorize(self._sigmoid)
+
+        A_matrix = np.array([[[self._calc_A(m, x, self.one_of_k(k)) for k in range(self.num_class)] for x in training_datas] for m in range(self.num_hidden)])
+        probs_matrix = np.array([self.probability(x) for x in training_datas])
+        sum_under_k = np.sum(A_matrix * probs_matrix, axis=2)
+
+        A_matrix_tx = np.array([[self._calc_A(m, training_datas[u], training_answers[u]) for u in range(len(training_datas))] for m in range(self.num_hidden)])
+        diff_v = np.dot( training_datas.T, (vsigmoid(A_matrix_tx) - sum_under_k).T)
+        return diff_v
+
+    def train(self, training, test, learning_time, batch_size, learning_rate=[0.01, 0.01, 0.01, 0.01], test_interval=100):
+        if not (self.num_visible == len(training.data[0])):
+            print(len(training.data[0]))
             raise TypeError
         
         for lt in range(learning_time):
-            self.probability(training_datas[0], update=True)
+            batch = training.minibatch(batch_size)
 
-            diff_b = self._differential_b(training_datas, training_answers)
-            diff_w = self._differential_w(training_datas, training_answers)
-            diff_c = self._differential_c(training_datas, training_answers)
-            diff_v = self._differential_v(training_datas, training_answers)
+            diff_b = self._differential_b(batch.data, batch.answer)
+            diff_w = self._differential_w(batch.data, batch.answer)
+            diff_c = self._differential_c(batch.data, batch.answer)
+            diff_v = self._differential_v(batch.data, batch.answer)
 
-            for k in range(self.num_class):
-                self.bias_b[k] += diff_b[k] * learning_rate[0]
+            self.bias_b += diff_b * learning_rate[0]
+            self.weight_w += diff_w * learning_rate[1]
+            self.bias_c += diff_c * learning_rate[2]
+            self.weight_v += diff_v * learning_rate[3]
             
-            for j in range(self.num_hidden):
-                for k in range(self.num_class):
-                    self.weight_w[j][k] += diff_w[j][k] * learning_rate[1]
-            
-            for j in range(self.num_hidden):
-                self.bias_c[j] += diff_c[j] * learning_rate[2]
-            
-            for i in range(self.num_visible):
-                for j in range(self.num_hidden):
-                    self.weight_v[i][j] += diff_v[i][j] * learning_rate[3]
-            
+            logging.info("️training is processing. {} / {}".format(lt+1, learning_time))
             if lt % test_interval == 0:
-                print("{} / {}".format(lt, len(training_answers)))
-                self.test(test_datas, test_answers)
+                self.test_error(test)
     
     def classify(self, input_data):
         probs = self.probability(input_data)
-        return probs.index(max(probs))
+        return np.argmax(probs)
     
-    def test(self, test_datas, test_answers):
-        correct = 0
-        for d, a in zip(test_datas, test_answers):
-            if a == self.classify(d):
-                correct += 1
-        print("correct rate: {}".format(correct / len(test_datas)))
+    def test_error(self, test):
+        classified_data = [self.one_of_k(self.classify(d)) for d in test.data]
+        correct = np.sum( np.dot(test.answer, classified_data.T) )
+        logging.info("️correct rate: {}".format(correct / float(len(test.data))))
+
+    def save(self, filename):
+        params = {
+            "num_class":self.num_class,
+            "num_hidden":self.num_hidden,
+            "num_visible":self.num_visible,
+            "bias_b":self.bias_b.tolist(),
+            "bias_c":self.bias_c.tolist(),
+            "weight_w":self.weight_w.tolist(),
+            "weight_v":self.weight_v.tolist(),
+        }
+        json.dump(params, open(filename, "w+"))
+    
+    @staticmethod
+    def load_from_json(filename):
+        params = json.load(open(filename, "r"))
+        drbm = DRBM(params["num_visible"], params["num_hidden"], params["num_class"])
+        drbm.bias_b = np.array(params["bias_b"])
+        drbm.bias_c = np.array(params["bias_c"])
+        drbm.weight_w = np.array(params["weight_w"])
+        drbm.weight_v = np.array(params["weight_v"])
+        return drbm
 
 def main():
-    drbm = DRBM(3, 5, 2)
-    print(drbm.probability([123,56,23]))
+    drbm = DRBM(3, 5, 7)
+    a = drbm.probability(np.array([123,56,23]))
+    print(a)
+    print(sum(a))
 
 if __name__=='__main__':
     main()
--- drbm_main.py
+++ drbm_main.py
@@ -3,31 +3,75 @@
 
 from DRBM import DRBM
 import time
+import numpy as np
+import logging
 
-vector_size = 784
-hidden_unit_num = 500
-class_num = 10
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+#np.seterr(all="raise")
 
-class MNIST:
-    def __init__(self, filename):
+class LearningData:
+    def __init__(self, data, answer):
+        self.data = data
+        self.answer = answer
+
+    def minibatch(self, batchsize):
+        length = len(self.data)
+        idx = np.random.choice(np.arange(0, length), size=batchsize, replace=False)
+        batch_data = np.take(self.data, idx, axis=0)
+        batch_answer = np.take(self.answer, idx, axis=0)
+        return LearningData(batch_data, batch_answer)
+
+class MNIST(LearningData):
+    def __init__(self, filename, num_class):
         self.data = []
         self.answer = []
         with open(filename, "r") as f:
             for line in f:
                 sp_line = line.split(",")
-                self.answer.append(DRBM.one_of_k(class_num, int(sp_line[0])))
+                self.answer.append(one_of_k(num_class, int(sp_line[0])))
                 self.data.append(list(map(lambda x: int(x), sp_line[1:])))
+        self.answer = np.array(self.answer)
+        self.data = np.array(self.data)
+    
+class dummy_data(LearningData):
+    def __init__(self, data_num, num_input, num_class):
+        self.answer = [one_of_k(num_class, i) for i in np.random.randint(0, num_class, data_num)]
+        self.data = np.random.randint(0, 256, (data_num, num_input))
+
+# one-of-k表現のベクトルを生成
+def one_of_k(num_class, number):
+    # "One of K" -> "ok"
+    ok = np.zeros(num_class)
+    ok.put(number, 1)
+    return ok
 
 def main():
-    drbm = DRBM(vector_size, hidden_unit_num, class_num)
-    train = MNIST("mnist_train.csv")
-    test = MNIST("mnist_test.csv")
+    vector_size = 784
+    hidden_unit_num = 500
+    class_num = 10
 
-    print("train started.")
+    drbm = DRBM(vector_size, hidden_unit_num, class_num)
+
+    # logging.info("creating dummy data.")
+    # train = dummy_data(100, vector_size, class_num)
+    # test = dummy_data(100, vector_size, class_num)
+    # logging.info("☑ creating dummy data complete.")
+
+    logging.info("️start loading data.")
+    train = MNIST("mnist_train.csv", class_num)
+    test = MNIST("mnist_test.csv", class_num)
+    logging.info("☑ loading data complete.")
+
+    logging.info("train started.")
     start_time = time.time()
-    drbm.train(train.data, train.answer, test.data, test.answer, 5000, test_interval=1)
+
+    drbm.train(train, test, 5000, 100)
+    
     end_time = time.time()
-    print("train end. time: {}sec".format(end_time-start_time))
+    logging.info("☑ train complete. time: {} sec".format(end_time-start_time))
+
+    drbm.save("parameters.json")
+    logging.info("☑ parameters dumped.")
 
 
 if __name__=='__main__':
