b9393fda44b116870d40ce1293090592174e576a
106 <Segmentation-fault@yandex.ua>
2020-01-29 17:06:09 +0900

    実行する初期パラメータの範囲を指定できるように変更

--- scripts/gengen/adamax_v20h100c10.sh
+++ scripts/gengen/adamax_v20h100c10.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-DIR="./results/"`date +%Y-%m-%d_%H-%M-%S`"_gengen_adamax_v20h100c10_sampling"
+if [ $# -ne 2 ]; then
+  echo "requires 2 arguments." 1>&2
+  exit 1
+fi
+
+DIR="./results/sgd_vs_adamax/generated_generative_model/"`date +%Y-%m-%d_%H-%M-%S`"_adamax_v20h100c10"
 mkdir -p $DIR
 
 data_num=500
@@ -10,7 +15,7 @@ learning_time=$(($data_num*$epoch/$batch_size))
 # 1epochごとにKLDやらの計算
 test_interval=$(($data_num/$batch_size))
 
-for file in `find ./params/v20h100c10_initial/ -type f | sort`
+for file in `find ./params/v20h100c10_initial/ -type f | sort | sed -n $1,$2p`
 do
     ./drbm_main.py $learning_time 0 ./settings/gengen_v20h100c10.json -s -l $data_num -i $test_interval -m $batch_size -d $DIR -g -a -p "adamax" -n $file
 done
