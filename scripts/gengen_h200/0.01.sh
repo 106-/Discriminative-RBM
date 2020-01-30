ed90795fc4e5a76dc98171c76d6d23c6a123375a
106 <Segmentation-fault@yandex.ua>
2020-01-30 16:54:08 +0900

    中間層200での実験設定を追加

---
+++ scripts/gengen_h200/0.01.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+  echo "requires 2 arguments." 1>&2
+  exit 1
+fi
+
+DIR="./results/sgd_vs_adamax/generated_generative_model_h200/"`date +%Y-%m-%d_%H-%M-%S`"_0.01_v20h200c10"
+mkdir -p $DIR
+
+data_num=500
+epoch=3000
+batch_size=50
+learning_time=$(($data_num*$epoch/$batch_size))
+# 1epochごとにKLDやらの計算
+test_interval=$(($data_num/$batch_size))
+
+for file in `find ./params/v20h200c10_initial/ -type f | sort | sed -n $1,$2p`
+do
+    ./drbm_main.py $learning_time 0 ./settings/gengen_v20h200c10.json -s -l $data_num -i $test_interval -m $batch_size -d $DIR -g -r 0.01 -p "0.01" -n $file
+done
