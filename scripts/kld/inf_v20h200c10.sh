0257751ebda1727df2a94c5b8c988d801d16970c
106 <Segmentation-fault@yandex.ua>
2020-01-31 12:46:53 +0900

    KLD計算のスクリプトを更新(Adamax使うようにしたり)

--- scripts/kld/inf_v20h200c10.sh
+++ scripts/kld/inf_v20h200c10.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-  echo "requires 1 arguments." 1>&2
+if [ $# -ne 2 ]; then
+  echo "requires 2 arguments." 1>&2
   exit 1
 fi
 
-DIR="./results/"`date +%Y-%m-%d_%H-%M-%S`"_N$1_inf_v20h200c10_sampling"
+DIR="./results/kld/"`date +%Y-%m-%d_%H-%M-%S`"_inf_v20h200c10"
 mkdir -p $DIR
 
 data_num=500
-epoch=1000
-batch_size=100
+epoch=3000
+batch_size=50
 learning_time=$(($data_num*$epoch/$batch_size))
 # 1epochごとにKLDやらの計算
 test_interval=$(($data_num/$batch_size))
 
-for i in `seq $1`
+for file in `find ./params/v20h200c10_initial/ -type f | sort | sed -n $1,$2p`
 do
-    ./drbm_main.py $learning_time 0 ./settings/v20h200c10.json -l $data_num -i $test_interval -m $batch_size -k ./params/gen_inf_v20h50c10.json -d $DIR
+    ./drbm_main.py $learning_time 0 ./settings/gengen_v20h200c10.json -l $data_num -i $test_interval -m $batch_size -d $DIR -g -p "d0_h200" -n $file
 done
--- scripts/kld/sparse_v20h200c10.sh
+++ scripts/kld/sparse_v20h200c10.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-  echo "requires 1 arguments." 1>&2
+if [ $# -ne 2 ]; then
+  echo "requires 2 arguments." 1>&2
   exit 1
 fi
 
-DIR="./results/"`date +%Y-%m-%d_%H-%M-%S`"_sparse_kld_v20h200c10"
+DIR="./results/kld/"`date +%Y-%m-%d_%H-%M-%S`"_sparse_v20h200c10"
 mkdir -p $DIR
 
 data_num=500
 epoch=3000
-batch_size=100
+batch_size=50
 learning_time=$(($data_num*$epoch/$batch_size))
 # 1epochごとにKLDやらの計算
 test_interval=$(($data_num/$batch_size))
 
-for i in `seq $1`
+for file in `find ./params/v20h200c10_initial/ -type f | sort | sed -n $1,$2p`
 do
-    ./drbm_main.py $learning_time 0 ./settings/v20h200c10.json -s -l $data_num -i $test_interval -m $batch_size -k ./params/gen_inf_v20h50c10.json -d $DIR
+    ./drbm_main.py $learning_time 0 ./settings/gengen_v20h200c10.json -s -l $data_num -i $test_interval -m $batch_size -d $DIR -g -a -p "s0_h200" -n $file
 done
