899fde2737ac1a3896ca962a3b942794e4383d84
106 <Segmentation-fault@yandex.ua>
2020-02-03 15:11:37 +0900

    ノイズ加算後クリップしない学習データの用意

---
+++ scripts/mnist/d120_unlimit/sparse_h100.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+  echo "requires 2 arguments." 1>&2
+  exit 1
+fi
+
+DIR="./results/mnist/"`date +%Y-%m-%d_%H-%M-%S`"_sparse_h100_MNIST_d120_unlimit"
+mkdir -p $DIR
+
+data_num=1000
+epoch=100
+batch_size=100
+learning_time=$(($data_num*$epoch/$batch_size))
+# 1epochごとにKLDやらの計算
+test_interval=$(($data_num/$batch_size))
+
+for file in `find ./params/v784h100c10_initial/ -type f | sort | sed -n $1,$2p`
+do
+    ./drbm_main.py $learning_time 0 ./settings/mnist_noise/d120_unlimit/h100.json -s -l $data_num -i $test_interval -m $batch_size -c -d $DIR -a -p "MNIST_sparse_h100" -n $file
+done
