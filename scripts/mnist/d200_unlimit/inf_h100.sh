60f3a8d8b263aeac5fbda7db33ab98b8b6ff41a3
106 <Segmentation-fault@yandex.ua>
2020-02-04 12:25:56 +0900

    分散200の設定を追加

---
+++ scripts/mnist/d200_unlimit/inf_h100.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+  echo "requires 2 arguments." 1>&2
+  exit 1
+fi
+
+DIR="./results/mnist/d200_unlimit/"`date +%Y-%m-%d_%H-%M-%S`"_inf_h100_MNIST_d200_unlimit"
+mkdir -p $DIR
+
+data_num=1000
+epoch=100
+batch_size=100
+learning_time=$(($data_num*$epoch/$batch_size))
+# 1epochごとにKLDやらの計算
+test_interval=$(($data_num/$batch_size))
+
+for file in `find ./params/v784h100c10_initial/ -type f | sort | sed -n $1,$2p`
+do
+    ./drbm_main.py $learning_time 0 ./settings/mnist_noise/d200_unlimit/h100.json -l $data_num -i $test_interval -m $batch_size -c -d $DIR -p "MNIST_inf_h100" -n $file
+done
---
+++ scripts/mnist/d200_unlimit/sparse_h200.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+  echo "requires 2 arguments." 1>&2
+  exit 1
+fi
+
+DIR="./results/mnist/d200_unlimit/"`date +%Y-%m-%d_%H-%M-%S`"_sparse_h200_MNIST_d200_unlimit"
+mkdir -p $DIR
+
+data_num=1000
+epoch=100
+batch_size=100
+learning_time=$(($data_num*$epoch/$batch_size))
+# 1epochごとにKLDやらの計算
+test_interval=$(($data_num/$batch_size))
+
+for file in `find ./params/v784h200c10_initial/ -type f | sort | sed -n $1,$2p`
+do
+    ./drbm_main.py $learning_time 0 ./settings/mnist_noise/d200_unlimit/h200.json -s -l $data_num -i $test_interval -m $batch_size -c -d $DIR -a -p "MNIST_sparse_h200" -n $file
+done
