95dc594fb3af87887f04cc07c503319774e6de98
106 <Segmentation-fault@yandex.ua>
2020-02-05 16:51:45 +0900

    スパースパラメータのヒストグラム作成用のスクリプトを追加

---
+++ scripts/histogram/sparse_h2000.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+  echo "requires 2 arguments." 1>&2
+  exit 1
+fi
+
+DIR="./results/histogram/"`date +%Y-%m-%d_%H-%M-%S`"_sparse_h2000"
+mkdir -p $DIR
+
+data_num=500
+epoch=3000
+batch_size=50
+learning_time=$(($data_num*$epoch/$batch_size))
+# KLDの計算をしない
+test_interval=$(($learning_time))
+
+for file in `find ./params/v20h2000c10_initial/ -type f | sort | sed -n $1,$2p`
+do
+    ./drbm_main.py $learning_time 0 ./settings/gengen/h2000.json -s -l $data_num -i $test_interval -m $batch_size -d $DIR -g -a -p "s0_h2000" -n $file
+done
